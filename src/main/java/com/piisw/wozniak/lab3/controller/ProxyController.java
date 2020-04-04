e76d925a8ebce8dc7b3db23dacaca991ceb81af7
Kacper <238069@student.pwr.edu.pl>
2020-04-04 18:57:37 +0200

    Lab 3 fix

--- src/main/java/com/piisw/wozniak/lab3/controller/ProxyController.java
+++ src/main/java/com/piisw/wozniak/lab3/controller/ProxyController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequestMapping ("rest")
 public class ProxyController {
--- src/main/java/com/piisw/wozniak/lab3/server/WiremockServers.java
+++ src/main/java/com/piisw/wozniak/lab3/server/WiremockServers.java
@@ -17,7 +17,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class WiremockServers {
 
-
         private Integer getPort;
         private Integer putPort;
         private Integer postPort;
